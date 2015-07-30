1d7493245e471bea625d7807cc19a74a5575783a
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2015-07-30 13:35:48 +0300

    Added possibility for user to name the progress_monitor table and process_control table

--- HUGOS-Instagram/base.py
+++ HUGOS-Instagram/base.py
@@ -1,4 +1,6 @@
 INSTANCE_NAME = 'your_instance_name_in_process_control_table'
+MONITOR_TABLE = 'insta_monitor'
+CONTROL_TABLE = 'insta_control'
 POSTGIS_DB_NAME = 'instagram'
 AWS_IP_ADDRESS = 'XX.XX.XXX.XXX'
 POSTGIS_PORT = 5432
